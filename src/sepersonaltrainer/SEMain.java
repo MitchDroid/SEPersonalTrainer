202aefdfd96cd01657e9b57ebfbe1f09cc6450a3
Miller Barrera <mjbarrerab@gmail.com>
2015-11-06 12:01:59 -0500

    :rabbit:
    :lantern:

---
+++ src/sepersonaltrainer/SEMain.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sepersonaltrainer;
+
+import ui.PersonalDataForm;
+
+/**
+ *
+ * @author miller.barrera
+ */
+public class SEMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new PersonalDataForm().setVisible(true);
+    }
+    
+}
